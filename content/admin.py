3915ff316f5ff3f65eda0f09ce14e0a660844780
Guido Maldonado <guidojmaldonado@gmail.com>
2024-01-18 13:09:38 -0500

    Added YouTubeVideo model to models.py & updated admin.py to register the model

--- content/admin.py
+++ content/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PodcastEpisode
+from .models import PodcastEpisode, YouTubeVideo
 
 @admin.register(PodcastEpisode)
 class PodcastEpisodeAdmin(admin.ModelAdmin):
     list_display = ('podcast_name', 'title', 'pub_date')
+
+@admin.register(YouTubeVideo)
+class YouTubeVideoAdmin(admin.ModelAdmin):
+    list_display = ('channel_name', 'title', 'upload_date')
--- content/models.py
+++ content/models.py
@@ -14,3 +14,17 @@ class PodcastEpisode(models.Model):
     def __str__(self) -> str:
         return f'{self.podcast_name}: {self.title}'
     
+class YouTubeVideo(models.Model):
+    title = models.CharField(max_length=200)
+    upload_date = models.DateTimeField()
+    channel_name = models.CharField(max_length=100)
+    channel_image = models.URLField()
+    video = models.URLField()
+    duration = models.CharField(max_length=50)
+    view_count = models.CharField(max_length=50)
+    like_ratio = models.CharField(max_length=50)
+    video_id = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.channel_name}: {self.title}'
+    
